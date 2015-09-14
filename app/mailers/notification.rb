4fb07aad8e63f8e40e88db4cbafc7bb335a00c2d
Eric Berry <cavneb@gmail.com>
2015-09-14 15:01:14 -0600

    removed zopim and added new recipients of email

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -4,7 +4,8 @@ class Notification < ApplicationMailer
   def contact(name, email, message)
     mail(
       from: "#{name} <#{email}>",
-      to: 'info@utaquaticsacademy.com',
+      to: ['info@utaquaticsacademy.com', 'jod@utaquaticsacademy.com', 'alliesha@utaquaticsacademy.com'],
+      bcc: ['amberry202@gmail.com'],
       subject: 'Email from Contact Page',
       body: message)
   end
