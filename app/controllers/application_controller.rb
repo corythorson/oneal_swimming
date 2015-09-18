05566ca075cf660af334169b1e599e47840eb678
Eric Berry <cavneb@gmail.com>
2015-09-18 16:28:20 -0600

    added iframe embedding support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :allow_iframe_requests
 
   def require_administrator
     unless current_user.try(:admin?)
       redirect_to root_url
     end
+  end
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
   end
 
 end
