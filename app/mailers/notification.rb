f9622c70b7a5c883ebf4cfa06e64fdb49e18995e
Eric Berry <cavneb@gmail.com>
2015-10-06 11:26:51 -0600

    updated mailer targets

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -4,7 +4,7 @@ class Notification < ApplicationMailer
   def contact(name, email, message)
     mail(
       from: "#{name} <#{email}>",
-      to: ['info@utaquaticsacademy.com', 'alliesha@utaquaticsacademy.com', 'alliesha_oneal@yahoo.com'],
+      to: ['info@utaquaticsacademy.com', 'alliesha_oneal@yahoo.com'],
       bcc: ['amberry202@gmail.com'],
       subject: 'Email from Contact Page',
       body: message)
