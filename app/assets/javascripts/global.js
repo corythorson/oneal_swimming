839a2347686e1372eb12e079f4b92287ec440949
Eric Berry <cavneb@gmail.com>
2016-05-07 21:37:20 -0600

    fixed js issue

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,9 +1,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
-  $('#global-modal').on('shown.bs.modal', function (e) {
-    var slider = $(".slider").bootstrapSlider();
-    slider.on("change", function (event) {
-      $('#quantity-label').html(event.value.newValue);
-    });
-  });
 });
