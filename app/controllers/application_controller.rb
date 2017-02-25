3ef50053ae6b45f0901f226831d951d69692263e
Eric Berry <cavneb@gmail.com>
2017-02-25 13:01:28 -0700

    Skip auth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
 
-  # before_action :allow_iframe_requests
+  before_action :allow_iframe_requests
 
   def require_administrator
     unless current_user.try(:admin?)
